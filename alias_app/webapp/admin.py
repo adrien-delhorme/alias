d27426f76e1f021c17985039948e266e09cd59ae
Adrien Delhorme <adrien.delhorme@gmail.com>
2016-01-16 22:57:15 +0100

    FEAT: register webapp models into django admin

--- alias_app/webapp/admin.py
+++ alias_app/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from alias_app.webapp.models import Settings
+from alias_app.webapp.models import HostingService
 
-# Register your models here.
+
+class SettingsAdmin(admin.ModelAdmin):
+    pass
+
+
+class HostingServiceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Settings, SettingsAdmin)
+admin.site.register(HostingService, HostingServiceAdmin)
